85e6685ba6d0ffbe3625048c14de2a677b9d16d2
Coop <cooperschwarting@icloud.com>
2024-01-20 15:07:52 -0600

    revert

--- src/main.cpp
+++ src/main.cpp
@@ -17,28 +17,28 @@ int getFrameIcon(IconType type) {
 }
 
 class $modify(MenuLayer) {
-    bool init() {
-        if(!MenuLayer::init())
-            return false;
+	bool init() {
+		if(!MenuLayer::init())
+			return false;
+		
+		auto gm = GameManager::sharedState();
 
-        auto gm = GameManager::sharedState();
-        auto profileMenu = this->getChildByID("profile-menu");
-        profileMenu->setLayout(AxisLayout::create());
-        profileMenu->setPositionX(this->getChildByID("player-username")->getPositionX());
+		auto profileMenu = this->getChildByID("profile-menu");
+		profileMenu->setLayout(AxisLayout::create());
+		profileMenu->setPositionX(this->getChildByID("player-username")->getPositionX());
 
-        SimplePlayer* playerIcon = SimplePlayer::create(0);
-        playerIcon->setColor(gm->colorForIdx(gm->getPlayerColor()));
-        playerIcon->setSecondColor(gm->colorForIdx(gm->getPlayerColor2()));
-        playerIcon->setGlowOutline(gm->colorForIdx(gm->getPlayerGlowColor()));
-        playerIcon->enableCustomGlowColor(gm->colorForIdx(gm->getPlayerGlowColor()));
-        if(!gm->getPlayerGlow()) playerIcon->disableGlowOutline();
-        playerIcon->updatePlayerFrame(static_cast<int>(getFrameIcon(gm->m_playerIconType)), static_cast<IconType>(gm->m_playerIconType));
-        playerIcon->setScale(1.15);
-
-        CCSprite* playerSprite = as<CCSprite*>(as<CCMenuItemSpriteExtra*>(profileMenu->getChildByID("profile-button"))->getChildren()->objectAtIndex(0));
-        playerSprite->addChild(playerIcon);
-        playerSprite->setDisplayFrame(playerIcon->displayFrame());
-
-        return true;
-    }
+		SimplePlayer* playerIcon = SimplePlayer::create(0);
+		playerIcon->setColor(gm->colorForIdx(gm->getPlayerColor()));
+		playerIcon->setSecondColor(gm->colorForIdx(gm->getPlayerColor2()));
+		playerIcon->setGlowOutline(gm->colorForIdx(gm->getPlayerGlowColor()));
+		playerIcon->enableCustomGlowColor(gm->colorForIdx(gm->getPlayerGlowColor()));
+		if(!gm->getPlayerGlow()) playerIcon->disableGlowOutline();
+		playerIcon->updatePlayerFrame(static_cast<int>(getFrameIcon(gm->m_playerIconType)), static_cast<IconType>(gm->m_playerIconType));
+		playerIcon->setScale(1.15);
+		CCSprite* playerSprite = as<CCSprite*>(as<CCMenuItemSpriteExtra*>(profileMenu->getChildByID("profile-button"))->getChildren()->objectAtIndex(0));
+		playerSprite->addChild(playerIcon);
+		playerSprite->setDisplayFrame(playerIcon->displayFrame());
+		
+		return true;
+	}
 };
